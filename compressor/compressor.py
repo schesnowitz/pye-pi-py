9045aade2b2f5edac4131880ab6701961878503c
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-03-01 15:51:24 -0500

    zipper app

--- compressor/compressor.py
+++ compressor/compressor.py
@@ -1,18 +1,30 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
-label_1 = sg.Text("Select files")
+label_1 = sg.Text("Select files         ")
 input_1 = sg.Input()
-choose_files_button_1 = sg.FilesBrowse("Choose")
+choose_files_button_1 = sg.FilesBrowse("Choose", key="files")
 
 label_2 = sg.Text("Select destination")
 input_2 = sg.Input()
-choose_files_button_2 = sg.FolderBrowse("Choose")
+choose_files_button_2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+
+output_message = sg.Text(key='message', text_color="lime")
 
 window = sg.Window("File Compressor",
                    layout=[[label_1, input_1, choose_files_button_1],
                            [label_2, input_2, choose_files_button_2],
-                           [compress_button]])
-window.read()
+                           [compress_button, output_message]])
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["message"].update(value="All files completed.")
+# sg.WIN_CLOSED:
 window.close()
---
+++ feet2met/sample.py
@@ -0,0 +1,32 @@
+# NOTE: This script runs only on your local IDE
+import PySimpleGUI as sg
+from converters import convert
+
+feet_label = sg.Text("Enter feet: ")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches: ")
+inches_input = sg.Input(key="inches")
+
+button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [button, output_label]])
+
+while True:
+    event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
+
+window.close()
+
+
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
