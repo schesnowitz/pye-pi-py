2171bc98d30d7cfb892f15125ec24bb48be34897
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-03-09 17:25:10 -0500

    streamlit

--- cam_motion/detect_motion.py
+++ cam_motion/detect_motion.py
@@ -1,19 +1,21 @@
 import cv2
 import time
-
+from emailer import send_email
 
 video = cv2.VideoCapture(0)
 # give cam time to load
 time.sleep(1)
 
 first_frame = None
+status_list = []
 while True:
+    status = 0
     check, frame = video.read()
     # set frames to BW as smaller
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                                         # amount of blur
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-    cv2.imshow("test video", gray_frame_gau)
+    # cv2.imshow("test video", gray_frame_gau)
 
     if first_frame is None:
         first_frame = gray_frame_gau
@@ -23,7 +25,7 @@ while True:
     # if pixel has a value more than 30 we set it to 255
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     dilate_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("test video", dilate_frame)
+    # cv2.imshow("test video", dilate_frame)
 
     contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -31,8 +33,28 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         x, y, width, height = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 3)
+        green_rectangle = cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 3)
+
+        if green_rectangle.any():
+            status = 1
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+    print(status_list)
+
+
+
+
+
     cv2.imshow("video", frame)
+
+
+
+
 
     # creates a kb exit when type 'q'
     key = cv2.waitKey(1)
